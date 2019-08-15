c1d7783e9cbca8ff10a75c9c205cd69600623391
zongzhenh <huangzongzhuan@gmail.com>
2019-08-15 18:06:30 +0800

    update

--- tmp_v0.09/index.js
+++
@@ -1,4 +0,0 @@
-const proxy = require('./lib/proxy');
-
-server = new proxy.WebSocketServer();
-server.runStatementProxy();
--- tmp_v1.0.0/demo.js
+++
@@ -1,8 +0,0 @@
-const child_process = require('child_process');
-
-const proc = child_process.spawnSync(`npm list -g | head -n 1`, {
-  shell: true,
-  encoding: 'utf8'
-});
-
-console.log('==================>', proc.stdout);
